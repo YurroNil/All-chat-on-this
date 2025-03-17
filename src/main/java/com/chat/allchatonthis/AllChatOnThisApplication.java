3c70c4f11acbb7e5985c61f8474af0febf370bc0
MilkWind <2250957906@qq.com>
2025-03-17 17:42:18 +0800

    init: 初始化

---
+++ src/main/java/com/chat/allchatonthis/AllChatOnThisApplication.java
@@ -0,0 +1,13 @@
+package com.chat.allchatonthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllChatOnThisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllChatOnThisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chat/allchatonthis/AllChatOnThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.allchatonthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllChatOnThisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
